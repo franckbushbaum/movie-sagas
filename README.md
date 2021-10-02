8c60800eb6edeb792f816b979c258784bf27b51a
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-10-02 18:09:16 -0500

    data inputs in AddMovie.jsx completed and tested.

--- README.md
+++ README.md
@@ -9,8 +9,15 @@ Your project description goes here. What problem did you solve? How did you solv
 - [x] Inventory of entire project.
 - [x] Create database
 - [x] Draw a diagram of Home page, Details page, and Add Movie page.
-- [] Create Details page, with back button to MovieList
+- [x] Create Details page, with back button to MovieList
 - [x] Create AddMovie page, with back button to MovieList
-- [] Create MovieItem. Route everything together.
-- [] Whiteboard schematic
+- [x] Create MovieItem. Route everything together.
+- [x] Whiteboard schematic
+- [] in /add. 
+    []Create 4 inputs. 3 go to 'movies' database. 1 of these sends to a Genre database. Test.
+    []Send movies data to Saga. Test.
+    []Create Function to post to server. Test.
+    []Create server post to movies Database. Test.
+    []Recreate for Genre database. This will take research.
+
 
--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,20 +1,78 @@
 import React from 'react';
-import { useState } from 'react'; 
-import { useDispatch } from 'react-redux'; 
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 
-function AddMovie(){
+function AddMovie() {
+//Data to be sent to Sagas.
+    const [movie, setMovie] = useState('');
 
-    const history = useHistory()
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+//If there is a way to combine setMovieTitle, setMoviePoster and setMovieDescription
+//I would love to see it. Spoiler: There absolutely is one.
+
+    const setMovieTitle = (event) => {
+        setMovie({
+            ...movie,
+            title: event.target.value
+        })
+    }
+
+    const setMoviePoster = (event) => {
+        setMovie({
+            ...movie,
+            poster: event.target.value
+        })
+    }
+
+    const setMovieDescription = (event) => {
+        setMovie({
+            ...movie,
+            description: event.target.value
+        })
+    }
+
+    // console.log(description);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('in handleSubmit')
+        dispatch({type: 'ADD_MOVIE', 
+                payload: movie})
+    }
 
     const backToHome = () => {
         history.push('/')
     }
 
-    return(
+    return (
         <>
-            <p>Under Construction</p>
-            <button  onClick={backToHome}>Cancel</button>
+            <p>Under Construction...</p>
+            <h2>Add a Movie</h2>
+            <form onSubmit={handleSubmit} className="add-movie-form">
+                <input 
+                    required
+                    placeholder="Title"
+                    value={movie.title}
+                    onChange={setMovieTitle}
+                />
+                 <input
+                    required
+                    placeholder="Poster"
+                    value={movie.poster}
+                    onChange={setMoviePoster}
+                />
+                <input
+                    required
+                    placeholder="Description"
+                    value={movie.description}
+                    onChange={setMovieDescription}
+                /> 
+                <input type='submit' value='Submit' />
+            </form>
+            <button onClick={backToHome}>Cancel</button>
         </>
 
 
